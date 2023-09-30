0d909e59c12d1b7e01ba83040ea182792d34335e
AntoshaLev <90862359+AntoshaLev@users.noreply.github.com>
2023-09-30 23:50:14 +0300

    lesson_5

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene.support.shared import browser
+from selene import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def config_browser():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.timeout = 2.0
+    browser.driver.set_window_size(1200, 1080)
+
+    yield
+
+    browser.quit()
---
+++ tests/test_filling_form.py
@@ -0,0 +1,37 @@
+import os
+from selene import browser, have, be, command
+import pytest
+
+
+def test_filling_form():
+    browser.open('/')
+    browser.element('#firstName').type('Anton')
+    browser.element('#lastName').type('Levochkin')
+    browser.element('#userEmail').type('www.Garu33@mail.ru')
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type('9998347268')
+    browser.element('#dateOfBirth').click()
+    browser.element(".react-datepicker__month-select").click().element('option[value="6"]').click()
+    browser.element(".react-datepicker__year-select").click().element('[value="1995"]').click()
+    browser.element(".react-datepicker__day--010").click()
+    browser.element('#subjectsInput').type('Computer Science').press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-3"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('file/1669713891808.jpg'))
+    browser.element('#currentAddress').type('adress')
+    browser.element('#react-select-3-input').type('Rajasthan').press_enter()
+    browser.element('#react-select-4-input').type('Jaiselmer').press_enter()
+    browser.element("#submit").click()
+    browser.element('#example-modal-sizes-title-lg').should(have.text("Thanks for submitting the form"))
+    browser.element('.table').all('tr td:nth-child(2)').should(have.texts(
+        'Anton Levochkin',
+        'www.Garu33@mail.ru',
+        'Male',
+        '9998347268',
+        '10 July,1995',
+        'Computer Science',
+        'Sports, Music',
+        '1669713891808.jpg',
+        'adress',
+        'Rajasthan Jaiselmer'
+    ))
