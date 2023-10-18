20cef4af0637f6133954aff1ab9f6cd2ef88c4d3
AntoshaLev <90862359+AntoshaLev@users.noreply.github.com>
2023-10-18 19:53:06 +0300

    lesson_10_part_1

--- tests/test_filling_form.py
+++ tests/test_filling_form.py
@@ -1,28 +1,29 @@
-import os
-from selene import browser, have
+from Lesson_10.pages.registration_page import RegistrationPage
+
+registration_page = RegistrationPage()
 
 
-def test_filling_form():
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').type('Anton')
-    browser.element('#lastName').type('Levochkin')
-    browser.element('#userEmail').type('www.Garu33@mail.ru')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('9998347268')
-    browser.element('#dateOfBirth').click()
-    browser.element(".react-datepicker__month-select").click().element('option[value="6"]').click()
-    browser.element(".react-datepicker__year-select").click().element('[value="1995"]').click()
-    browser.element(".react-datepicker__day--010").click()
-    browser.element('#subjectsInput').type('Computer Science').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('file/1669713891808.jpg'))
-    browser.element('#currentAddress').type('adress')
-    browser.element("#state").click().element("#react-select-3-option-1").click()
-    browser.element("#city").click().element("#react-select-4-option-0").click()
-    browser.element("#submit").click()
-    browser.element('#example-modal-sizes-title-lg').should(have.text("Thanks for submitting the form"))
-    browser.element('.table').all('tr td:nth-child(2)').should(have.texts(
+def test_student_registration_form():
+    # GIVEN
+    registration_page.open()
+
+    # WHEN
+    registration_page.fill_first_name('Anton')
+    registration_page.fill_last_name('Levochkin')
+    registration_page.fill_email('www.Garu33@mail.ru')
+    registration_page.choose_a_gender()
+    registration_page.fill_phone_number('9998347268')
+    registration_page.choose_date_of_birth(month='7', year='96', day='010')
+    registration_page.choose_a_subject('Computer Science')
+    registration_page.choose_a_hobby()
+    registration_page.upload_a_picture('1669713891808.jpg')
+    registration_page.type_current_address('adress')
+    registration_page.choose_state('Uttar Pradesh')
+    registration_page.choose_city('Agra')
+    registration_page.submit_form()
+
+    # THEN
+    registration_page.student_should_by_registred(
         'Anton Levochkin',
         'www.Garu33@mail.ru',
         'Male',
@@ -33,4 +34,4 @@ def test_filling_form():
         '1669713891808.jpg',
         'adress',
         'Uttar Pradesh Agra'
-    ))
+    )
